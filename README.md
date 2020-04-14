7cb9290b86d1d466786551c1ecb8567523cc9304
Milja Lempinen <milja.lempinen@gmail.com>
2020-04-14 19:59:07 +0300

    frontend

---
+++ README.md
@@ -0,0 +1,1 @@
+https://telephone-ex.herokuapp.com/api/persons
---
+++ build/precache-manifest.a4f5f585289c0a19b5d0c5e153df5747.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9993fb4b6ea1f650a76edd0a1d9fdd51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e367f7780edfa713a8e",
+    "url": "/static/css/main.fb8a9dfa.chunk.css"
+  },
+  {
+    "revision": "b4451df55eb7b9398cec",
+    "url": "/static/js/2.e53cb1f4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.e53cb1f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e367f7780edfa713a8e",
+    "url": "/static/js/main.7b526922.chunk.js"
+  },
+  {
+    "revision": "3e7a3a712b9d49c8dff0",
+    "url": "/static/js/runtime-main.ecb5677e.js"
+  }
+]);
